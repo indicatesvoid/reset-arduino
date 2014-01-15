5938fb09691bb2214ad1c645ea0675fceba2a652
William Clark <william.a.clark@gmail.com>
2014-01-15 14:56:54 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+####reset-arduino
+##Reset an Arduino over the command line
+
+---
+
+Currently only supports Mac OS. Windows support forthcoming.
+
+No command-line flags. Forthcoming: verbose option, set pulse width.
--- reset-arduino/main.cpp
+++ reset-arduino/main.cpp
@@ -1,5 +1,5 @@
 //
-//  main.c
+//  main.cpp
 //  reset-arduino
 //
 //  Created by William Clark on 1/15/14.
@@ -7,12 +7,20 @@
 //
 
 #include <stdio.h>
+#include "Arduino.h"
+
+Arduino* arduino = new Arduino();
 
 int main(int argc, const char * argv[])
 {
+    // connect at 9600 baud
+    arduino->connect(9600);
+    
+    // attempt reset
+    arduino->reset();
 
     // insert code here...
-    printf("Hello, World!\n");
+    // printf("Hello, World!\n");
     return 0;
 }
 
