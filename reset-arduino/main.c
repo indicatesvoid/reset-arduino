19acc3fbe4cb18a9492cdb97305ce6ab3726f9d5
William Clark <william.a.clark@gmail.com>
2014-01-15 14:20:42 -0800

    Initial Commit

---
+++ reset-arduino/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  reset-arduino
+//
+//  Created by William Clark on 1/15/14.
+//  Copyright (c) 2014 William Clark. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
